192bcd9ec5c5b6cec26a24b784542887be05d090
viniciusalvessouza <viniciusalves.souza@yahoo.com.br>
2023-05-14 04:16:24 -0300

    components dinamicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string ='https://folhagospel.com/wp-content/uploads/2017/08/banda_resgate_no_seu_quintal_capa_20171.jpg'
+  
+  @Input()
+  cardTitle:string = 'Banda Resgate a asdasda'
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
